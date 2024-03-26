25a94742d28726c7759809bac0fe3f48a761f6f6
Mikael <mikaelhadler@gmail.com>
2024-03-25 23:03:05 -0300

    up files playground submodule

---
+++ github-oauth/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from './components/Login'
+import './App.css'
+import './globals.css'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center">
+      <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ github-oauth/src/components/Login.tsx
@@ -0,0 +1,33 @@
+import { Button } from "./ui/button"
+import { Icons } from "./ui/icons"
+import { Input } from "./ui/input"
+
+const Login = () => {
+  return (
+    <div className="flex flex-col gap-3 w-[350px] mx-auto">
+        <h1 className="text-2xl font-bold">Create an account</h1>
+        <h2 className="text-slate-500 text-sm mb-2">Enter your email below to create your account</h2>
+        
+        <Input placeholder="name@example.com" />
+        <Button
+            type="button"
+            className="text-sm font-normal">Sign In with Email</Button>
+        <div className="uppercase text-xs text-slate-500 my-2">
+            or continue with
+        </div>
+        <Button variant={'outline'} className="font-normal">
+            {Icons.gitHub({
+                style: {
+                    width: 16,
+                    height: 16,
+                    marginRight: 8
+                }
+            })}
+            Github
+        </Button>
+
+    </div>
+  )
+}
+
+export default Login
